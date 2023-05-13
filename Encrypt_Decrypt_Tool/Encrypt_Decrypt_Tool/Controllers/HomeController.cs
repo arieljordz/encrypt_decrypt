519ee59b3f5a475acbbc3793e0e06f92f932bf1b
Ariel Sueno <arieljordz@gmail.com>
2023-05-14 01:08:26 +0800

    first commit

---
+++ Encrypt_Decrypt_Tool/Encrypt_Decrypt_Tool/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypt_Decrypt_Tool.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private static string GetHashKey()
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("1", "Concentrix-JET2022!");
+            return ht["1"].ToString();
+        }
+
+        /// <summary>
+        /// Encrypt a string using dual encryption method. Return a encrypted cipher Text
+        /// </summary>
+        /// <param name="toEncrypt">string to be encrypted</param>
+        /// <param name="useHashing">use hashing? send to for extra secirity</param>
+        /// <returns></returns>
+        public static string Encrypt(string toEncrypt, bool useHashing)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+
+            string key = GetHashKey();
+            if (useHashing)
+            {
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                hashmd5.Clear();
+            }
+            else
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            tdes.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        /// <summary>
+        /// DeCrypt a string using dual encryption method. Return a DeCrypted clear string
+        /// </summary>
+        /// <param name="cipherString">encrypted string</param>
+        /// <param name="useHashing">Did you use hashing to encrypt this data? pass true is yes</param>
+        /// <returns></returns>
+        public static string Decrypt(string cipherString, bool useHashing)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = Convert.FromBase64String(cipherString);
+
+            string key = GetHashKey();
+            if (useHashing)
+            {
+                MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                hashmd5.Clear();
+            }
+            else
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+            tdes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            tdes.Clear();
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
---
+++ Encrypt_Decrypt_Tool/Encrypt_Decrypt_Tool/Scripts/main.js
@@ -0,0 +1,36 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+function Encrypt() {
+    $("#Input").val();
+    $.ajax({
+        url: "../HomeController/Encrypt",
+        type: 'POST',
+        data: { toEncrypt: $("#Input").val(), useHashing: true},
+        success: function (res) {
+            if (res.success == true) {
+                $("#Output").val("sdasdaEncrypt");
+            } else {
+                $("#Input").val("werwer");
+            }
+        }
+    });
+}
+function Decrypt() {
+    $("#Input").val();
+    $.ajax({
+        url: "../HomeController/Decrypt",
+        type: 'POST',
+        data: { cipherString: $("#Input").val(), useHashing: true },
+        success: function (res) {
+            if (res.success == true) {
+                $("#Output").val("sdasdaDecrypt");
+            } else {
+                $("#Input").val("werwer");
+            }
+        }
+    });
+}
